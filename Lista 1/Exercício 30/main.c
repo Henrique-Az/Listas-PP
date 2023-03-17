c9041490ee4e17b06832940b51dbaa14aa341645
Henrique Azevedo Silva <127798220+Henrique-Az@users.noreply.github.com>
2023-03-17 10:10:29 -0300

    Add files via upload

---
+++ Lista 1/Exercício 30/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float d, r, c;
+
+    printf("Insira o preco em reais:\n");
+    scanf("%f", &r);
+
+    printf("Insira a cotacao do dolar no momento:\n");
+    scanf("%f", &c);
+
+    d=r/c;
+    printf("O preco inserido, em dolar, eh %f", d);
+
+    return 0;
+
+
+}
